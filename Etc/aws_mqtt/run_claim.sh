8056b380bf7f659204e90ff9eb710384f87e4f86
mhhong <armkernel8@gmail.com>
2023-10-17 17:12:01 +0900

    fix minor

--- Etc/aws_mqtt/run_claim.sh
+++ Etc/aws_mqtt/run_claim.sh
@@ -1,3 +1,4 @@
 export LD_LIBRARY_PATH=/root/lib
 rdate -s time.bora.net
 ./fleet-provisioning --endpoint a1diue5dwjnn4d-ats.iot.ap-northeast-2.amazonaws.com --ca_file ./AmazonRootCA1.pem --cert ./claim_certificate.pem.crt --key ./claim_private.pem.key --template_name test-provisioning-template --template_parameters '{"SerialNumber":"iMON","ThingName":"iMON"}'
+sync
